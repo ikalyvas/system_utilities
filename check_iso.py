dcd393f43f530081347bd3cc782371148e1a6765
ioannis kalyvas <jkalivas@gmail.com>
2016-03-02 00:16:32 +0200

    autopep8 fixes

--- check_iso.py
+++ check_iso.py
@@ -1,10 +1,10 @@
-B#!/usr/bin/python
+#!/usr/bin/python
 
 import urllib2
 import sys
 import re
 from time import sleep
-i0;115;0cmport smtplib
+import smtplib
 import ldap
 from getpass import getuser as username
 from email.mime.text import MIMEText
@@ -12,53 +12,52 @@ from email.mime.text import MIMEText
 
 class Message(Exception):
 
-
-    def __init__(self,*args):
-        super(Message,self).__init__(*args)
+    def __init__(self, *args):
+        super(Message, self).__init__(*args)
         self.send_mail(*args)
 
-
-    def send_mail(self,*args):
-       
+    def send_mail(self, *args):
         """
         Function that will send out a descriptive message
         to me whether the iso succeded,or failed.
         """
         outgoing_message = args[0]
         if self.__class__ == SuccessIso:
-            outgoing_message += '\n'+send_description()
+            outgoing_message += '\n' + send_description()
 
-      
         sender = find_sender(username())
-        receivers = [sender] + [find_sender(people) for people in optional_people if optional_people]
+        receivers = [sender] + [find_sender(people)
+                                for people in optional_people if optional_people]
         msg = MIMEText(outgoing_message)
 
-        msg['Subject'] = 'ISO trigger...' 
+        msg['Subject'] = 'ISO trigger...'
         msg['From'] = sender
         msg['To'] = ','.join(receivers)
-        s=smtplib.SMTP('localhost')
-        s.sendmail(sender,[sender,','.join(receivers)],msg.as_string())
+        s = smtplib.SMTP('localhost')
+        s.sendmail(sender, [sender, ','.join(receivers)], msg.as_string())
         s.quit()
         sys.exit()
-
-
-
 
 
 class FatalError(Message):
     pass
 
+
 class DownloadError(Message):
     pass
+
 
 class ParsingError(Message):
     pass
 
+
 class RevisionError(Message):
     pass
 
+
 class NotCompletedError(Message):
     pass
+
 
 class SuccessIso(Message):
     pass
@@ -66,11 +65,10 @@ class SuccessIso(Message):
 
 def init_globals():
 
-
     global commited
     global url
     global timer
-    global countdown 
+    global countdown
     global branch
     global optional_people
 
@@ -80,48 +78,47 @@ def init_globals():
         optional_people = sys.argv[3:]
     except IndexError:
         pass
-    url='http://hugin.dev.cic.org-rdnet.net/scit/html/projects/ISO_'+branch+'_delivery_AB4/index.html'
-    timer = 900 #15minutes
-    countdown = 7200 #2hours
+    url = 'http://hugin.dev.cic.org-rdnet.net/scit/html/projects/ISO_' + \
+        branch + '_delivery_AB4/index.html'
+    timer = 900  # 15minutes
+    countdown = 7200  # 2hours
+
 
 def validate():
-    if len(sys.argv)<3:
+    if len(sys.argv) < 3:
         print 'Tool accepts 2 mandatory arguments'
         print 'The branch and the revision'
         sys.exit(2)
 
+
 def find_sender(user):
-
-
     """
         Utility to find the mail address of the sender
 
     """
 
-
-
     l = ldap.open("ed-p-gl.emea.org-net.net")
-    l.protocol_version=ldap.VERSION3
+    l.protocol_version = ldap.VERSION3
     baseDN = "o=org"
     searchScope = ldap.SCOPE_SUBTREE
-    retrieveAttributes=None
-    searchFilter= "uid="+str(user.strip())
-    x= l.search(baseDN,searchScope,searchFilter,retrieveAttributes)
-    res = l.result(x,0)
+    retrieveAttributes = None
+    searchFilter = "uid=" + str(user.strip())
+    x = l.search(baseDN, searchScope, searchFilter, retrieveAttributes)
+    res = l.result(x, 0)
     l.unbind_s()
     return res[1][0][1].get('mail')[0]
 
 
 def get_page(urlname):
 
-    i=0    #number of retries until give up getting the page of scit
-    while (i != 20): 
+    i = 0  # number of retries until give up getting the page of scit
+    while (i != 20):
         try:
             resp = urllib2.urlopen(urlname).readlines()
         except urllib2.HTTPError as e:
             print e
-            sleep(60)#wait 1 minute and retry again
-            i+=1
+            sleep(60)  # wait 1 minute and retry again
+            i += 1
         else:
             return resp
 
@@ -129,30 +126,26 @@ def get_page(urlname):
                          \n\nUnable to download it for further parsing' % url)
 
 
-
 def get_latest_triggered(resp):
-
 
     for line in resp:
         if 'Latest triggered revision' not in line:
             continue
         else:
             try:
-                revision = re.search('Latest triggered revision.+/(\d{6})',\
-                                      line).group(1)
+                revision = re.search('Latest triggered revision.+/(\d{6})',
+                                     line).group(1)
             except:
                 raise ParsingError('Found Latest triggered revision\
                                     but could not retrieve it...')
             else:
-                return revision  
+                return revision
 
     raise ParsingError('Could not find at all \
                         Latest triggered revision')
 
 
-
 def verify_triggered_equals_commited(triggered):
-
     """returns True if the revision 
        from which the latest ISO has been triggered
        is the same as the commited one"""
@@ -161,16 +154,15 @@ def verify_triggered_equals_commited(triggered):
 
 
 def check_ISO_progress(url_response):
-    
     """this function should run in a loop until the ISO ends.."""
 
     for line in url_response:
         if 'running' in line and 'abortbuild' not in line:
             print 'ISO has been started with  %s running...' % commited
-            print 'Timer started for 15m' 
-            sleep(timer) #sleep and wake up to check if it has completed
+            print 'Timer started for 15m'
+            sleep(timer)  # sleep and wake up to check if it has completed
             print 'Woke up.Lets loop again'
-            break    
+            break
         elif 'Latest completed revision' in line and 'success' in line and commited in line:
             raise SuccessIso('ISO has been successfully completed.')
 
@@ -179,91 +171,80 @@ def check_ISO_progress(url_response):
 
 
 def get_page_and_triggered():
-
-
     """1)Fetch the url from scit
        2)Parse it to find the latest triggered revision
        3)Check if the current triggered is the same as 
          the recently commited
        4)Return the url page and the result of 3)"""
 
-    
     page = get_page(url)
 
     latest_triggered_revision = get_latest_triggered(page)
- 
+
     verified = verify_triggered_equals_commited(latest_triggered_revision)
 
-    return verified,page
-
-
+    return verified, page
 
 
 def wait_until_completion(page):
-
     """Loop that runs maximum up to 2h.
        During this timeframe the script sleeps
        for 15m and wakes up to check if the ISO
        has been completed"""
 
-
     global countdown
-    while (countdown != 0):   
+    while (countdown != 0):
 
         check_ISO_progress(page)
         page = get_page(url)
-        countdown -= timer #start gradually reducing by 15m the whole duration 2h...
+        # start gradually reducing by 15m the whole duration 2h...
+        countdown -= timer
 
-    check_ISO_progress(page) #do one last check just in case...
+    check_ISO_progress(page)  # do one last check just in case...
     raise NotCompletedError('ISO has not completed in the usual timeframe...')
 
 
-
 def send_description():
-
     """Finds the ISO name on success"""
-
 
     hugin = 'http://hugin.dev.cic.org-rdnet.net'
     for line in urllib2.urlopen(url):
-        if 'Latest completed revision' in line and 'success' in line and commited in line:   
-            success_link = re.search('href=("\/scit\/html\/ISO.+)>success',line).group(1).strip('"')
+        if 'Latest completed revision' in line and 'success' in line and commited in line:
+            success_link = re.search(
+                'href=("\/scit\/html\/ISO.+)>success', line).group(1).strip('"')
             success_link = hugin + success_link
-            success_link = success_link.replace('.html','_log')
-            success_link = success_link+'/'+re.search('AB4_(.+)_log',success_link).group(1)+'_3:ISO_delivery_'+branch+'_AB4_s1'
+            success_link = success_link.replace('.html', '_log')
+            success_link = success_link + '/' + \
+                re.search('AB4_(.+)_log', success_link).group(1) + \
+                '_3:ISO_delivery_' + branch + '_AB4_s1'
 
             for line in urllib2.urlopen(success_link):
                 if 'Sending command: scp -v -t' in line:
-                    iso_name = re.search('scp -v -t (.+)',line).group(1)
+                    iso_name = re.search('scp -v -t (.+)', line).group(1)
                     return iso_name
- 
 
 
 def fail_and_exit():
-
-
     """i believe its self documented"""
 
-    raise FatalError('ISO should have been triggered with %s.Unknown fatal error' % commited)
+    raise FatalError(
+        'ISO should have been triggered with %s.Unknown fatal error' % commited)
 
 
 def main():
-
 
     validate()
 
     init_globals()
 
-    sleep(timer) # give some time for the command to be scheduled...
+    sleep(timer)  # give some time for the command to be scheduled...
 
-    is_triggered , page = get_page_and_triggered()
-
+    is_triggered, page = get_page_and_triggered()
 
     if is_triggered:
         wait_until_completion(page)
     else:
         fail_and_exit()
-
 
 
 if __name__ == '__main__':
--- colorize.py
+++ colorize.py
@@ -5,30 +5,31 @@ import sys
 import itertools
 import gzip
 import os
-from subprocess import Popen,STDOUT,PIPE
+from subprocess import Popen, STDOUT, PIPE
 
-crit=re.compile(r'(?<=crit) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
-err=re.compile(r'(?<=err) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
-warn=re.compile(r'(?<=warn) (CLA-\d|AS\d+-\d)|SAB\d+-\d|IB\d+-\d')
-info=re.compile(r'(?<=info) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
-notice=re.compile(r'(?<=notice) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
-debug =re.compile(r'(?<=debug) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
+crit = re.compile(r'(?<=crit) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
+err = re.compile(r'(?<=err) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
+warn = re.compile(r'(?<=warn) (CLA-\d|AS\d+-\d)|SAB\d+-\d|IB\d+-\d')
+info = re.compile(r'(?<=info) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
+notice = re.compile(r'(?<=notice) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
+debug = re.compile(r'(?<=debug) (CLA-\d|AS\d+-\d|SAB\d+-\d|IB\d+-\d)')
 other_err = re.compile(r'(?<=err) (\w.+)')
 
 #:it adjusts the scrolling page into your screen :#
 #rows,colums = os.popen('stty size','r').read().split()
 
 #:color db :#
-red='\033[91m'
-white='\033[97m'
-yellow='\033[93m'
-cyan='\033[94m'
-green='\033[92m'
-pink ='\033[95m'
+red = '\033[91m'
+white = '\033[97m'
+yellow = '\033[93m'
+cyan = '\033[94m'
+green = '\033[92m'
+pink = '\033[95m'
 
-def write(_line,color=None):
 
-    sys.stdout.write(color+_line+''+'\n')
+def write(_line, color=None):
+
+    sys.stdout.write(color + _line + '' + '\n')
     sys.stdout.flush()
 
 
@@ -39,59 +40,59 @@ def tail_lines(pipe):
     if line == '':
         sys.exit(4)
     elif info.search(line):
-        write(line,white)
+        write(line, white)
     elif err.search(line):
-        write(line,red)
+        write(line, red)
     elif warn.search(line):
-        write(line,yellow)         
+        write(line, yellow)
     elif notice.search(line):
-        write(line,cyan)
+        write(line, cyan)
     elif debug.search(line):
-        write(line,green)
+        write(line, green)
     elif other_err.search(line):
-        write(line,red)
+        write(line, red)
     elif crit.search(line):
-        write(line,pink)
+        write(line, pink)
     else:
         pass
-                    
 
 
 def print_lines():
 
-    for next_lines in itertools.islice(f,41):
+    for next_lines in itertools.islice(f, 41):
         if next_lines:
             for line in next_lines.splitlines():
                 if info.search(line):
-                    print white+line+''
+                    print white + line + ''
                 elif err.search(line):
-                    print red+line+''
+                    print red + line + ''
                 elif warn.search(line):
-                    print yellow+line+''
+                    print yellow + line + ''
                 elif notice.search(line):
-                    print cyan+line+''
+                    print cyan + line + ''
                 elif debug.search(line):
-                    print green+line+''
+                    print green + line + ''
                 elif other_err.search(line):
-                    print red+line+''
+                    print red + line + ''
                 elif crit.search(line):
-                    print pink+line+''
+                    print pink + line + ''
                 else:
                     pass
         else:
             break
 
 
-
-syslog=sys.argv[1]
+syslog = sys.argv[1]
 
 try:
     if syslog == 'master-syslog':
-        answer=raw_input('For tail -f press y.\nOtherwise it will open master-syslog from the beginning:')
+        answer = raw_input(
+            'For tail -f press y.\nOtherwise it will open master-syslog from the beginning:')
         if answer == 'y' or answer == 'Y':
-            p = Popen('tail -f '+syslog,shell=True,stdout=PIPE,stderr=STDOUT) 
+            p = Popen('tail -f ' + syslog, shell=True,
+                      stdout=PIPE, stderr=STDOUT)
             while True:
-                try:    
+                try:
                     tail_lines(p)
                 except KeyboardInterrupt:
                     sys.exit(2)
@@ -99,19 +100,17 @@ try:
             f = open(syslog)
     else:
         if syslog.endswith('gz'):
-            f=gzip.open(syslog,'rb')
+            f = gzip.open(syslog, 'rb')
         else:
             f = open(syslog)
 
-    
     while True:
         try:
             raw_input()
             print_lines()
         except KeyboardInterrupt:
             sys.exit(4)
-        
+
 except IOError:
     print 'File %s does not exist' % syslog
     sys.exit(3)
-
