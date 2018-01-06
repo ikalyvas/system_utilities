f4ab17c3c79e224f11fb70c7e79a42322feb76bb
rainman <rainman@git.com>
2018-01-06 17:02:17 +0200

    working solution that treat vlans as integers.Execution time is close to 12seconds

--- telnyx/VlanAssigner.py
+++ telnyx/VlanAssigner.py
@@ -45,58 +45,59 @@ class VlanAssigner(object):
         _ = next(vlanReader)
         for line in vlanReader:
             device_id, primary_port, vlan_id = line
-            vlan_obj = Vlan(vlan_id,device_id)
+            vlan_id = int(vlan_id)
             primary_port = int(primary_port)
 
             if primary_port:
-                self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].append(vlan_obj)
+                self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].append(vlan_id)
             else:
-                self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE].append(vlan_obj)
+                self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE].append(vlan_id)
 
     def sort_vlan_pools(self):
 
         for device_id in self.deviceId_to_port_vlans.keys():
-            self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].sort(key=operator.attrgetter('vlanId'))
-            self.deviceId_to_port_vlans[device_id][self.PRIMARY_RESERVED].sort(key=operator.attrgetter('vlanId'))
-            self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE].sort(key=operator.attrgetter('vlanId'))
-            self.deviceId_to_port_vlans[device_id][self.SECONDARY_RESERVED].sort(key=operator.attrgetter('vlanId'))
+            self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].sort()
+            self.deviceId_to_port_vlans[device_id][self.PRIMARY_RESERVED].sort()
+            self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE].sort()
+            self.deviceId_to_port_vlans[device_id][self.SECONDARY_RESERVED].sort()
 
     def find_available_port(self, req_id, is_redundant_request=False):
 
-        device_ids = sorted(self.deviceId_to_port_vlans.keys())
+        free_pairs = []
 
         if is_redundant_request:
-            for device_id in device_ids:
-                for vlan_obj in self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE]:
-                    free_pair = [(vlan_obj,sec_vlan_obj) for sec_vlan_obj in self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE]
-                                  if vlan_obj.vlanId == sec_vlan_obj.vlanId]
-                    if free_pair:
+            for device_id in self.deviceId_to_port_vlans.iterkeys():
+                    common_set = set(self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE]) & \
+                              set(self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE])
+                    if common_set:
+                        free_pairs.append((device_id, min(common_set)))
 
-                            self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].remove(vlan_obj)
-                            self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE].remove(sec_vlan_obj)
-                            self.deviceId_to_port_vlans[device_id][self.PRIMARY_RESERVED].append(vlan_obj)
-                            self.deviceId_to_port_vlans[device_id][self.SECONDARY_RESERVED].append(sec_vlan_obj)
-                            self.output_file.write(', '.join([req_id, device_id, self.PRIMARY_PORT, vlan_obj.vlanId])+'\n')
-                            self.output_file.write(', '.join([req_id, device_id, self.SECONDARY_PORT, sec_vlan_obj.vlanId])+'\n')
-                            return
-
-            else:
-                raise Exception("Does not have pair primary/secondary ports for the redundant request")
-
+            free_pairs.sort(key=lambda x: (x[1], x[0]))
+            device_id, vlan_id = free_pairs[0]
+            self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].remove(vlan_id)
+            self.deviceId_to_port_vlans[device_id][self.SECONDARY_FREE].remove(vlan_id)
+            self.deviceId_to_port_vlans[device_id][self.PRIMARY_RESERVED].append(vlan_id)
+            self.deviceId_to_port_vlans[device_id][self.SECONDARY_RESERVED].append(vlan_id)
+            self.output_file.write(','.join([req_id, device_id, self.SECONDARY_PORT, str(vlan_id)])+'\n')
+            self.output_file.write(','.join([req_id, device_id, self.PRIMARY_PORT, str(vlan_id)])+'\n')
 
         else:
 
             lowest_vlans_per_device = []
-            for device_id in device_ids:
-                lowest_vlans_per_device.append(self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE][0])
-            lowest_vlans_per_device.sort(key=operator.attrgetter('vlanId','deviceId'))
-            vlan_obj = lowest_vlans_per_device[0]
-            self.deviceId_to_port_vlans[vlan_obj.deviceId][self.PRIMARY_FREE].remove(vlan_obj)
-            self.deviceId_to_port_vlans[vlan_obj.deviceId][self.PRIMARY_RESERVED].append(vlan_obj)
-            self.output_file.write(', '.join([req_id, vlan_obj.deviceId, self.PRIMARY_PORT, vlan_obj.vlanId])+'\n')
+            for device_id in self.deviceId_to_port_vlans.iterkeys():
+                lowest_vlans_per_device.append((device_id,
+                                                self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE][0])
+                                               )
+            lowest_vlans_per_device.sort(key=lambda x:(x[1], x[0]))
+            device_id,vlan_id = lowest_vlans_per_device[0]
+            self.deviceId_to_port_vlans[device_id][self.PRIMARY_FREE].remove(vlan_id)
+            self.deviceId_to_port_vlans[device_id][self.PRIMARY_RESERVED].append(vlan_id)
+            self.output_file.write(','.join([req_id, device_id, self.PRIMARY_PORT, str(vlan_id)])+'\n')
 
 
 if __name__ == '__main__':
+    import time
+    start = time.time()
     vlans = sys.argv[1]
     requests = sys.argv[2]
     assigner = VlanAssigner(vlans)
@@ -106,3 +107,5 @@ if __name__ == '__main__':
     for line in requests_reader:
         req_id , is_redundant = line
         assigner.find_available_port(req_id,int(is_redundant))
+    end = time.time()
+    print 'Execution took %d' % (end-start)
